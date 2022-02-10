936c82448fcf842de88b5bd7018faae154a44b36
ruimihinyakku <zm19407@josai.ac.jp>
2022-02-10 12:20:13 +0000

    0210

---
+++ r2.js
@@ -0,0 +1,6 @@
+function txtchange1(){
+    document.getElementById('txt1').innerHTML='須賀川市';
+    }
+    function imgchange1(){
+      document.getElementById('logo').src='sukagawa.png';
+    }
---
+++ r4.js
@@ -0,0 +1,14 @@
+function 厚焼き玉子66() {
+    who66 = '厚焼き玉子'
+    alert('正解');
+  }
+  
+  function サーモン66() {
+    who66 = 'サーモン'
+    alert('不正解');
+  }
+  
+  function まぐろ66() {
+     who66 = 'まぐろ'
+    alert('正解');
+  }
